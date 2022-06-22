d1afbf7e9492d6fcb3d25cce69ade8b4dbb6d0f0
Anastasia Berger <nastya2905@gmail.com>
2022-06-22 20:04:00 +0300

    ClientController anotation edit

--- src/main/java/com/jb/CouponSystem/controllers/ClientController.java
+++ src/main/java/com/jb/CouponSystem/controllers/ClientController.java
@@ -8,11 +8,11 @@ import com.jb.CouponSystem.login.TokenManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("customer")
+@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*", allowCredentials = "true")
 public abstract class ClientController {
 
     @Autowired
